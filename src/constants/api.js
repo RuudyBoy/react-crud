b5a660b529c32f2b67b8e901430ee6aa4b274ce7
RuudyBoy <73653923+RuudyBoy@users.noreply.github.com>
2022-06-08 23:42:05 +0200

    f

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "http://organicflowerpower.org/wp-json/wp/v2/posts"
-export const TOKEN_PATH = "http://localhost/project-xam/wp-jon/jwt-auth/v1/token";
+export const TOKEN_PATH = "http://localhost/project-exam/wp-jon/jwt-auth/v1/token";
 
